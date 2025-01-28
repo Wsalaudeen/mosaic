73c95c975effda3855bdfbe030493df85ce43174
Faizah-Olasumbo <olasumbooladunni@87@gmail.com>
2025-01-28 18:41:27 +0000

    feature-base

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,12 +1,18 @@
+import Explore from "./Explore";
+import Footer from "./generalComponent/Footer/Footer";
 import Navbar from "./generalComponent/Navbar/Navbar";
 import Hero from "./Hero/Hero";
 import HeroSection from "./HeroSection/HeroSection";
+import Testimonial from "./Testimonial";
 export default function LandingPage() {
   return (
     <>
       <Navbar />
       <Hero />
       <HeroSection />
+      <Explore />
+      <Testimonial />
+      <Footer />
     </>
   );
 }
