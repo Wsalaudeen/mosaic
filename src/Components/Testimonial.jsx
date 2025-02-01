e255464465423f85cffa06dbf21e60936bb00c49
theOlanrewaju <olanrewajulanzy@gmail.com>
2025-02-01 10:24:50 +0100

    final landing page

--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -65,7 +65,7 @@ function Testimonial() {
       </div>
 
       <div className="">
-        <img src={Wave} alt="" />
+        <img src={Wave} alt="wave" className="w-full" />
         <div className=" bg-blue-200">
           <div className="flex flex-col items-center justify-center text-center">
             <p className="lg:text-3xl text-xl font-bold text-text-black mb-5 mt-10">
